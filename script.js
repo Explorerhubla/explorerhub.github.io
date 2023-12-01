028f1f4f3cf2eec4d7c47cdce8302f585b5d1894
Explorerhubla <explorerhubla@gmail.com>
2023-12-01 01:06:38 -0800

    all

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    
-    hamburgerMenu.addEventListener('click', function () {
-        this.classList.toggle('clicked');
-    });
-});
